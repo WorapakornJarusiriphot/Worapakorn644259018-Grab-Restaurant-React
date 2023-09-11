45e0cec1e3f0bb6e7a20a41cd9752422c0cf9a59
worapakorn <pupupuwora@gmail.com>
2023-09-11 12:56:52 +0700

    Initial commit

---
+++ src/pages/Restaurant.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const URL = import.meta.env.VITE_BASE_URL;
+const USERNAME = import.meta.env.VITE_BASE_USERNAME;
+const PASSWORD = import.meta.env.VITE_BASE_PASSWORD;
+const config = {
+  auth: {
+    username: USERNAME,
+    password: PASSWORD,
+  },
+};
+
+const Restaurant = () => {
+  const [restarants, setRestaurants] = useState([]);
+  useEffect(() => {
+    const fetchAllRestaurants = async () => {
+      try {
+        const res = await axios.get(`${URL}/restaurants`, config);
+        setRestaurants(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchAllRestaurants();
+  }, []);
+  return (
+    <div>
+      <h1>Restaurant</h1>
+    </div>
+  );
+};
+
+export default Restaurant;
